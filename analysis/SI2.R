7deaa0e6f25f04a6f0e0c6069a1fb8e847876d20
Joe Roe <git@joeroe.io>
2022-12-23 07:58:02 +0100

    Add references, plant part

--- analysis/SI2.R
+++ analysis/SI2.R
@@ -1,10 +1,16 @@
 # Draft table for SI2
 flora |>
   drop_na(founder_crop) |>
-  group_by(founder_crop, period, site_name, phase_code) |>
+  mutate(plant_part = if_else(str_detect(taxon_source, "grain"), "grain", "undifferentiated")) |>
+  group_by(founder_crop, period, site_name, phase_code, plant_part) |>
   summarise(
     age = first(paste0(age_start, "–", age_end, " BP")),
     total_remains = sum(n),
+    references = paste(reference, collapse = ";")
   ) |>
+  mutate(
+    references = map(str_split(references, ";"), unique),
+    references = map_chr(references, paste, collapse = "; ")
+  )|>
   write_csv("analysis/figures/SI2_table.csv")
 
